2db91a81a9403be980f3b03aa867bf983fb5cbb1
binfengxueqian <42887408+binfengxueqian@users.noreply.github.com>
2020-02-22 09:23:41 +0800

    基本功能实现 可以建立文件属性数据库 可以检查文件是否被修改，是否为新增文件，是否被删除
    添加README.md
    添加忽略文件
    
    完善结构

--- lib/Config.py
+++ lib/Config.py
@@ -1,13 +1,17 @@
 import os
 
 # 项目目录
-ProjectDir = '/home/craipy/Project/ORM-LinuxSecurity/'
+ProjectDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # 数据存放目录
 dataDir = os.path.join(ProjectDir,'data')
+
 # 初始数据库路径
 initDB_Path = os.path.join(dataDir,'init.db')
+
 # 配置文件存放目录
 etcDir = os.path.join(ProjectDir,'etc')
+
 # 配置文件路径
 policyPath = os.path.join(etcDir,'pol.txt')
 
--- tset.py
+++ tset.py
@@ -1,5 +1,3 @@
-from lib.MyStat import *
-a = Mystat('os.stat_result(st_mode=33188, st_ino=103, st_dev=2055, st_nlink=1, st_uid=0, st_gid=0, st_size=314036, st_atime=1582266462, st_mtime=1541720608, st_ctime=1577205451)')
-print(a.__dict__)
-
+from lib.Config import *
+print(ProjectDir)
 
